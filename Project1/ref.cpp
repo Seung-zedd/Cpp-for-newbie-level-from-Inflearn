65b3add1b171cac53e244f575af44e8afc433dc4
Seung je Cho <csz0209@gmail.com>
2022-09-10 13:01:19 +0900

    2022.09.10 1시 참조 변수 학습 완료

---
+++ Project1/ref.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+void swapA(int&, int&);
+void swapB(int*, int*);
+void swapC(int, int);
+
+//swap함수:값에 의한 복사, 참조에 의한 복사, 포인터에 의한 복사
+
+int main()
+{
+	int wallet1 = 100, wallet2 = 200;
+	
+	cout << "최초의 값\n";
+	cout <<"wallet1 ="<< wallet1 << ", wallet2 =" << wallet2 << endl;
+
+	cout << "참조의 의한 값의 교환\n";
+	swapA(wallet1, wallet2);
+	cout << "wallet1 =" << wallet1 << ", wallet2 =" << wallet2 << endl;
+
+	cout << "포인터를 이용한 값의 교환\n";
+	swapB(&wallet1, &wallet2);
+	cout << "wallet1 =" << wallet1 << ", wallet2 =" << wallet2 << endl;
+
+	cout << "값을 이용한 값의 교환\n";
+	swapC(wallet1, wallet2);
+	cout << "wallet1 =" << wallet1 << ", wallet2 =" << wallet2 << endl;
+
+
+
+	return 0;
+}
+
+void swapA(int &a, int &b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void swapB(int* a, int* b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+void swapC(int a, int b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+
+}
